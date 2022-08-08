c769a724cd39d388654901c839427cf145194455
Yago Gomes Da Silva <99800161+YagoGomesDaSilva@users.noreply.github.com>
2022-08-08 13:16:49 -0300

    Commit 3

--- estudo django/formulario_django - forms/formularios/views.py
+++ estudo django/formulario_django - forms/formularios/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import formularioCadastro
 
 def home(request):
-    return render(request,'home.html')
+    form = formularioCadastro()
+    return render(request,'home.html',{'form':form})
 
 def processa_formulario(request):
-    nome = request.POST.get("nome")
-    email = request.POST.get("email")
-    return HttpResponse(f'{nome} {email}')
+    
+    return HttpResponse('teste')
 
 
